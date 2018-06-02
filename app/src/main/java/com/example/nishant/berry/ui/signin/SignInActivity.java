86b5cbe5b023c18802a5d2afe926f3589e4d9e6b
Nishant Patel <nishantuwindsor@gmail.com>
2018-06-01 20:16:19 -0400

    Refractor login -> signIn

--- app/src/main/java/com/example/nishant/berry/ui/signin/SignInActivity.java
+++ app/src/main/java/com/example/nishant/berry/ui/signin/SignInActivity.java
@@ -23,18 +23,18 @@
  * Last Modified on 01/06/18 8:11 PM
  */
 
-package com.example.nishant.berry.ui.login;
+package com.example.nishant.berry.ui.signin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.nishant.berry.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_in);
     }
 }
