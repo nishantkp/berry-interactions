0b3890da0517d462f09541dc7d9ecdf78eba074b
Nishant Patel <nishantuwindsor@gmail.com>
2018-06-09 15:53:00 -0400

    Add firebase function to send notification

---
+++ firebaseFunction/index.js
@@ -0,0 +1,35 @@
+'use-strict'
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.sendNotification = functions.database.ref('/notifications/{userId}/{notificationId}').onWrite((change, context) => {
+
+  //const notification = event.params.notification;
+
+  console.log('we have a notification to send to : ', context.params.userId);
+
+  // If the notification is deleted from database, we don't want to send it to user
+  // So simly return from there
+  if(!change.after.exists()){
+    return console.log('A notification has been deleted from database : ',context.params.notificationId);
+  }
+
+  return admin.database().ref(`/users/${context.params.userId}/token_id`)
+                        .once('value').then(function(snapshot) {
+
+    const tokenId = snapshot.val();
+    console.log('token_id'. tokenId);
+
+    const payload = {
+      notification: {
+        title: "Friend Request",
+        body: "You have received a new Friend Request",
+        icon: "default"
+      }
+    };
+
+    return admin.messaging().sendToDevice(tokenId, payload);
+  });
+});
