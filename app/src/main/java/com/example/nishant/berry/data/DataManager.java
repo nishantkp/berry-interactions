5fa8fb3b5e02d0d5b12344953f6427227bef2778
Nishant Patel <nishantuwindsor@gmail.com>
2018-06-24 13:22:43 -0400

    Create data manager singleton to deal with business logic

---
+++ app/src/main/java/com/example/nishant/berry/data/DataManager.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2018
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * File Created on 24/06/18 1:18 PM by nishant
+ * Last Modified on 24/06/18 1:18 PM
+ */
+
+package com.example.nishant.berry.data;
+
+import android.content.Context;
+
+/**
+ * Data Manager class, that deals with business logic
+ */
+public class DataManager {
+    private static DataManager sDataManager;
+
+    // Singleton
+    public static DataManager getInstance(Context context) {
+        if (sDataManager == null) {
+            sDataManager = new DataManager();
+        }
+        return sDataManager;
+    }
+}
